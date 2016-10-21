e86376d8a49a51ec77fb2b37f26651464c7e77d8
Lai Sim <laisim@netccentric.com>
2016-10-21 16:31:00 +0800

    Remove the complicated part of flattening the array, because the previous few commits have already fixed this

--- src/MassMailerParameter.php
+++ src/MassMailerParameter.php
@@ -18,16 +18,10 @@ class MassMailerParameter
      */ 
     public static function create( Request $request, MassMailerOptions $mailer_options )
     {
-        $attributes = $request -> input('data');
-
-        // To flatten the array
-        $compiled_attributes = [];
-        for( $x = 0; $x < count($attributes); $x++ ){
-            $compiled_attributes = array_merge( $compiled_attributes, $attributes[$x] );
-        }       
+        $attributes = $request -> input('data');  
 
         // Create the MassMailerParams object to hold all of the attributes & other related information
-        $mailer_params = MassMailerParams::create( ['attributes' => $compiled_attributes] );
+        $mailer_params = MassMailerParams::create( ['attributes' => $attributes] );
 
         // Dealing with the presenter parameters & put it into the MassMailerParams
         $presenter = MassMailerPresenter::create( $mailer_options -> presenter, $mailer_params );
